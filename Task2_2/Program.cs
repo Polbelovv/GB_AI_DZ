4e90965146e54a84731d605a54a6ed0ab0b4bd1f
Pavel <belou1828@gmail.com>
2022-08-16 21:28:43 +0300

    change

---
+++ Task2_2/Program.cs
@@ -0,0 +1,12 @@
+System.Console.WriteLine("Случайное трёхзначное число без средней цифры");
+int value1, value2, value3;
+int num = new Random().Next(100, 999);
+System.Console.WriteLine("Случайное трёхзначное число до преобразования:");
+System.Console.WriteLine($"> {num}");
+
+value1 = num % 10;
+value2 = num / 10 % 10;
+value3 = num / 100 % 10;
+System.Console.WriteLine("Случайное трёхзначное число после преобразования:");
+System.Console.Write($"> {value3}");
+System.Console.Write($"{value1}");
