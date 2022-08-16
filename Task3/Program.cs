8b291b8759756f368af3db1099ad035684821976
Pavel <belou1828@gmail.com>
2022-08-16 11:07:27 +0300

    add files task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Promt(string message)
+{
+
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+System.Console.WriteLine("Введите число для выяснения четности");
+int value1 = Promt("Введите испытуемое число > ");
+//int value2 = Promt("Введите второе число > ");
+if(value1 % 2 == 0)
+    System.Console.WriteLine($"{value1} is an even number");
+else
+    System.Console.WriteLine($"{value1} is an odd number");
