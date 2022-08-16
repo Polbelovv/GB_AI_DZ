c70e47f1364cb056e8a52407a15915953d355909
Pavel <belou1828@gmail.com>
2022-08-16 17:46:37 +0300

    add files Task2_1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Promt(string message)
+{
+
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+
+System.Console.WriteLine("Введите трёхзначное число");
+int value1 = Promt("Введите число > ");
+int value2 = 0;
+
+value2 = value1 / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа: {value2}");
