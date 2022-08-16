1eff1f0dc5ad5f9e458adab1c5c377209a07fc6a
Pavel <belou1828@gmail.com>
2022-08-16 21:55:25 +0300

    add files Task2_4

---
+++ Task2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int Promt(string message)
+{
+
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+System.Console.WriteLine("Введите номер дня недели");
+int value1 = Promt("Введите число > ");
+//int value2 = Promt("Введите второе число > ");
+if(value1 != 6 && value1 != 7)
+    System.Console.WriteLine("День рабочий");
+else
+    System.Console.WriteLine("День выходной");
