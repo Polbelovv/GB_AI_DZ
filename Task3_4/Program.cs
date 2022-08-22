1f98a0e1ef68a8884aa9f7060ebaaf1d0036a2a0
Pavel <belou1828@gmail.com>
2022-08-22 14:16:22 +0300

    add files Task3_4*

---
+++ Task3_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// сортировка массива вставками
+
+int Promt(string message)
+{
+    
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+void Sort_insert(int[] arrA)
+{
+    for(int i=1;i<10;i++){
+		for(int j=i; j>0 && arrA[j-1]>arrA[j];j--){
+			int tmp=arrA[j-1];
+			arrA[j-1]=arrA[j];
+			arrA[j]=tmp;
+		}
+    }
+   int count = arrA.Length;
+    for(int i = 0; i < count; i++)  
+        Console.WriteLine(arrA[i]);
+}
+int [] arrA = new int[10];
+System.Console.WriteLine("Введите случайные 10 чисел:");
+for (int i = 0; i < 10; i++){
+    arrA[i] = Promt("Введите числа для заполнения массива А > ");
+}
+
+Sort_insert(arrA);
+
+
+
