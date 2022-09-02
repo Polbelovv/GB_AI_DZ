6aaefbe9f1f3c2b6557868035b5962d055580969
Pavel <belou1828@gmail.com>
2022-09-02 14:37:03 +0300

    add new info Program

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int Promt(string message)
+{
+    
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+int Even_number(int[] arrA)
+{   
+    int count = 0;
+    for(int i=0;i<5;i++)
+    {
+        if(arrA[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int [] arrA = new int[5];
+System.Console.WriteLine("Введите случайные трёхзначные 5 чисел:");
+for (int i = 0; i < 5; i++){
+    arrA[i] = Promt("Введите числа для заполнения массива А > ");
+}
+System.Console.WriteLine("Количество четных чисел в массиве:"); 
+        int n = Even_number(arrA);
+        Console.WriteLine(n);
+
